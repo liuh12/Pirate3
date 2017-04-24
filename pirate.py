3f79636720f367d4dd0603c2622cbc3c4b2ed748
liuh12 <liuh12@janelia.hhmi.org>
2017-04-24 15:58:22 -0400

    Print out treaure contents

--- pirate.py
+++ pirate.py
@@ -1,4 +1,8 @@
 
-print "Arr, matey"
+print("Arr, matey")
 for i in range(100):
-     print str(i) + "bottles of rum"
+	print str(i) + "bottles of rum"
+
+with open("treasure.txt") as fh:
+ 	for line in fh:
+		print(line)
